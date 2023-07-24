764bb7e7a424a369d1423f6bc9cc0a8a98aaee4e
Lazy_V <954294627@qq.com>
2023-07-24 20:32:56 +0100

    chore: format code.

--- Source/Renderer.h
+++ Source/Renderer.h
@@ -5,20 +5,20 @@
 
 namespace VRaytracer
 {
-	class Renderer
-	{
+    class Renderer
+    {
     public:
         static bool Init();
         static void Release();
-        
-		static void Render(const std::shared_ptr<FrameBuffer>& frameBuffer);
 
-		static void Clear(float r, float g, float b, float a);
+        static void Render(const std::shared_ptr<FrameBuffer>& frameBuffer);
+
+        static void Clear(float r, float g, float b, float a);
         static void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
 
-		static uint32_t GetRenderTextureID() { return s_RenderTextureID; }
+        static uint32_t GetRenderTextureID() { return s_RenderTextureID; }
 
-	private:
+    private:
         static uint32_t s_RenderTextureID;
-	};
-}
+    };
+} // namespace VRaytracer
